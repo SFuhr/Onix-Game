dda88c4c6efd46c1195f5572306021fd83f74f13
SFuhr <54191619+SFuhr@users.noreply.github.com>
2020-11-21 17:27:37 +0200

    First Commit

---
+++ Assets/Scripts/Level/MainGrid.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+
+namespace Level
+{
+    public class MainGrid : MonoBehaviour
+    {
+        [SerializeField] private MeshRenderer gridMesh;
+        [SerializeField] private MeshRenderer cellPrefab;
+
+        private Texture _texture;
+        private Material _material;
+        private SpriteRenderer _sprite;
+
+        private void Start()
+        {
+            // Test();
+        }
+
+        private void Test()
+        {
+            _material = gridMesh.material;
+            _texture = _material.mainTexture;
+
+            var width = _texture.width;
+            var height = _texture.height;
+            
+            Debug.Log($"Texture size: {width.ToString()}x{height.ToString()}");
+
+            var zeroPoint = gridMesh.bounds.min;
+
+            float step = .02f;
+
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    var cell = new Vector3(zeroPoint.x + x * step,zeroPoint.y + y * step,0);
+                    Instantiate(cellPrefab, cell, Quaternion.identity,transform);
+                }
+            }
+        }
+    }
+}
