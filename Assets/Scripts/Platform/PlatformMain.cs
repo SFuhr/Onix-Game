d3feacdedd7ee983f1f5c41a1d737f5d7d82d1c2
SFuhr <54191619+SFuhr@users.noreply.github.com>
2021-02-06 09:53:01 +0200

    Revert "Not working"
    This reverts commit 80fb1d18a07c105a4a92fd35bbbe7b0c3b392221.

--- Assets/Scripts/Platform/PlatformMain.cs
+++ Assets/Scripts/Platform/PlatformMain.cs
@@ -9,7 +9,7 @@ namespace Platform
     public class PlatformMain : MonoBehaviour
     {
         [Header("Settings")]
-        [SerializeField] private float xBorder = 5f;
+        [SerializeField] private float maxXOffset = 5f;
         [SerializeField] private float speed = 10f;
         [SerializeField] private float lowestPoint = -95f;
         [Header("Elements")]
@@ -17,8 +17,6 @@ namespace Platform
         [SerializeField] private LayerMask scanLayer;
         [SerializeField] [Range(0,1)] private float scanTolerance = 0.9f;
         [SerializeField] private float growthSpeed = 2f;
-
-        public static float XBorderSize { get; private set; }
         
         private bool _isRunning;
         private float _xAxis;
@@ -28,7 +26,6 @@ namespace Platform
         
         private void Start()
         {
-            XBorderSize = xBorder;
             _scanResults = new Collider2D[10];
             var element = Instantiate(elementPrefab, transform.position, Quaternion.identity,transform);
             element.SetOwnerPlatform(this);
@@ -50,7 +47,7 @@ namespace Platform
             if(!_isRunning) return;
             
             var pos = transform.position;
-            pos.x = Mathf.Clamp(pos.x + _xAxis, -xBorder,xBorder);
+            pos.x = Mathf.Clamp(pos.x + _xAxis, -maxXOffset,maxXOffset);
             pos.y -= Time.deltaTime * speed;
 
             transform.position = pos;
@@ -79,7 +76,7 @@ namespace Platform
                     if (!col.isTrigger)
                     {
                         // separate element here
-                        var elemBounds = elem.GetBounds();
+                        var elemBounds = elem.GetBounds;
                         var colBounds = col.bounds;
                         
                         if (elemBounds.min.x < colBounds.min.x)
